1cfc42757e7a0b717456023c4599cc48fc399830
pradeepmistrydev <chauhanpradeep341@gmail.com>
2023-12-27 11:30:39 +0530

    add shop now

--- src/components/hero_banner.js
+++ src/components/hero_banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../style.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router-dom';
 
 
 const HeroBanner = () => {
@@ -11,7 +12,9 @@ const HeroBanner = () => {
                     <img src="images/glares.webp" className="w-100 banner_img" alt="banner" />
                     <div className='banner_content'>
                         <h2>Fashion Changing Always</h2>
-                        <button className='btn btn-primary shop_nowbtn'>Shop Now</button>
+                        <NavLink to="/ProductsCategory">
+                            <button className='btn btn-primary shop_nowbtn'>Shop Now</button>
+                        </NavLink>
                     </div>
 
                 </div>
